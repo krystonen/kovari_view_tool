5ada8c911a797f64b7c28b59193cc4db2954d6bb
Krisztina Kovari <krystonen@gmail.com>
2019-02-26 13:05:48 +0100

    Rebuilding gem

---
+++ lib/kovari_view_tool.rb
@@ -0,0 +1,7 @@
+require "kovari_view_tool/version"
+require "kovari_view_tool/renderer"
+
+module KovariViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kovari_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KovariViewTool
+  VERSION = "0.1.0"
+end
